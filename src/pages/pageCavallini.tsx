97d11b949c794b147cc07a536cb9c2035a2bdf6c
CavalliniMattia <mattia.cavallini18@gmail.com>
2023-11-06 15:31:30 +0100

    soluzione scritta file aggiornato

--- src/pages/pageCavallini.tsx
+++ src/pages/pageCavallini.tsx
@@ -3,6 +3,12 @@ const PageCavallini = () => {
   return (
     <div>
       <Logo />
+      <p>
+        dopo aver creato una nuova branch master-copia, ho eseguito un "git
+        reset 6d4dc95" nella master per tornare alla versione dopo il merge,
+        quindi pulita senza modifiche al file pageCavallini e senza la creazione
+        del file Logo.tsx
+      </p>
     </div>
   );
 };
