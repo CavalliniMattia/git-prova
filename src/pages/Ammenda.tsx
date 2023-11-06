fef0a111d6398332c7f0794523eb306895af755c
CavalliniMattia <mattia.cavallini18@gmail.com>
2023-11-06 17:08:27 +0100

    scusa

---
+++ src/pages/Ammenda.tsx
@@ -0,0 +1,14 @@
+const Ammenda = () => {
+  return (
+    <div>
+      <p>
+        ho eseguito un commit ma mi sono accorto che ho sbagliato a scrivere il
+        messaggio, come posso correggerlo? soluzione: dopo aver fatto la commit
+        con messaggio sbagliato posso fare il comando git commit --amend per
+        riscrivere il messaggio della commit
+      </p>
+    </div>
+  );
+};
+
+export default Ammenda;
