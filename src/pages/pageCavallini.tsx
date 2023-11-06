700ac45bad8410766c8d6b8e8b109caf35a13773
CavalliniMattia <mattia.cavallini18@gmail.com>
2023-11-06 15:47:14 +0100

    file modificato con soluzione

--- src/pages/pageCavallini.tsx
+++ src/pages/pageCavallini.tsx
@@ -2,6 +2,13 @@ const PageCavallini = () => {
   return (
     <div>
       <h1>Mattia Cavallini</h1>
+      <p>
+        la mia branch dedicata è indietro di qualche commit rispetto a master,
+        come faccio a portarla avanti? Cioè, come faccio a portare le modifiche
+        fatte su master sulla mia branch dedicata? soluzione: faccio un "git
+        merge master" così da apportare le modifiche fatte a master sulla mia
+        branch di adesso cavallini-init
+      </p>
     </div>
   );
 };
